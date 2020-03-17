da95cf2d2335a6cc72b4f663140206543a6f7620
Ryusuke Nomoto <r.nmt000@gmail.com>
2020-03-17 22:44:56 +0900

    Use props for all atribute

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.css';
 import CommentDetail from "./CommentDetail";
+import faker from 'faker';
 
 function App() {
   return (
     <div className="ui container comments">
-      <CommentDetail author="Sam"/>
-        <CommentDetail author="John"/>
-        <CommentDetail author="James"/>
+      <CommentDetail author="Sam" created_at="2020/03/19" content="content1" avatar={faker.image.avatar()}/>
+        <CommentDetail author="John" created_at="2020/03/11" content="content2" avatar={faker.image.avatar()}/>
+        <CommentDetail author="James" created_at="2020/02/01" content="content3" avatar={faker.image.avatar()}/>
     </div>
   );
 }
